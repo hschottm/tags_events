df36acd161432844a8146cf163758992c3d48f83
Helmut Schottmüller <helmut.schottmueller@me.com>
2013-11-08 19:00:01 +0100

    Added Contao 3.2 compatibility

--- classes/TagListEvents.php
+++ classes/TagListEvents.php
@@ -6,8 +6,8 @@ namespace Contao;
  * Class TagListEvents
  *
  * Provide methods to handle tag input fields.
- * @copyright  Helmut Schottmüller 2009
- * @author     Helmut Schottmüller <typolight@aurealis.de>
+ * @copyright  Helmut Schottmüller 2009-2013
+ * @author     Helmut Schottmüller <https://github.com/hschottm>
  * @package    Controller
  */
 class TagListEvents extends TagList
@@ -22,9 +22,9 @@ class TagListEvents extends TagList
 		$ids = array();
 		for ($i = 0; $i < count($for_tags); $i++)
 		{
-			$arr = $this->Database->prepare("SELECT DISTINCT tl_tag.id FROM tl_tag, tl_calendar_events WHERE tl_tag.id = tl_calendar_events.id AND tl_calendar_events.pid IN ('" . join($this->arrCalendars, "','") . "') AND from_table = ? AND tag = ?" . (!BE_USER_LOGGED_IN ? " AND (start='' OR start<?) AND (stop='' OR stop>?) AND published=1" : "") . " ORDER BY tl_tag.id ASC")
+			$arr = $this->Database->prepare("SELECT DISTINCT tl_tag.tid FROM tl_tag, tl_calendar_events WHERE tl_tag.tid = tl_calendar_events.id AND tl_calendar_events.pid IN ('" . join($this->arrCalendars, "','") . "') AND from_table = ? AND tag = ?" . (!BE_USER_LOGGED_IN ? " AND (start='' OR start<?) AND (stop='' OR stop>?) AND published=1" : "") . " ORDER BY tl_tag.tid ASC")
 				->execute(array('tl_calendar_events', $for_tags[$i], time(), time()))
-				->fetchEach('id');
+				->fetchEach('tid');
 			if ($i == 0)
 			{
 				$ids = $arr;
@@ -38,7 +38,7 @@ class TagListEvents extends TagList
 		$arrCloudTags = array();
 		if (count($ids))
 		{
-			$objTags = $this->Database->prepare("SELECT tag, COUNT(tag) as count FROM tl_tag, tl_calendar_events WHERE tl_tag.id = tl_calendar_events.id AND tl_calendar_events.pid IN ('" . join($this->arrCalendars, "','") . "') AND from_table = ? AND tl_tag.id IN (" . join($ids, ",") . ")" . (!BE_USER_LOGGED_IN ? " AND (start='' OR start<?) AND (stop='' OR stop>?) AND published=1" : "") . " GROUP BY tag ORDER BY tag ASC")
+			$objTags = $this->Database->prepare("SELECT tag, COUNT(tag) as count FROM tl_tag, tl_calendar_events WHERE tl_tag.tid = tl_calendar_events.id AND tl_calendar_events.pid IN ('" . join($this->arrCalendars, "','") . "') AND from_table = ? AND tl_tag.tid IN (" . join($ids, ",") . ")" . (!BE_USER_LOGGED_IN ? " AND (start='' OR start<?) AND (stop='' OR stop>?) AND published=1" : "") . " GROUP BY tag ORDER BY tag ASC")
 				->execute('tl_calendar_events', time(), time());
 			$list = "";
 			$tags = array();
@@ -48,7 +48,7 @@ class TagListEvents extends TagList
 				{
 					if (!in_array($objTags->tag, $for_tags))
 					{
-						$count = count($this->Database->prepare("SELECT tl_tag.id FROM tl_tag, tl_calendar_events WHERE tl_tag.id = tl_calendar_events.id AND tl_calendar_events.pid IN ('" . join($this->arrCalendars, "','") . "') AND tag = ? AND from_table = ?" . (!BE_USER_LOGGED_IN ? " AND (start='' OR start<?) AND (stop='' OR stop>?) AND published=1" : "") . " AND tl_tag.id IN (" . join($ids, ",") . ")")
+						$count = count($this->Database->prepare("SELECT tl_tag.tid FROM tl_tag, tl_calendar_events WHERE tl_tag.tid = tl_calendar_events.id AND tl_calendar_events.pid IN ('" . join($this->arrCalendars, "','") . "') AND tag = ? AND from_table = ?" . (!BE_USER_LOGGED_IN ? " AND (start='' OR start<?) AND (stop='' OR stop>?) AND published=1" : "") . " AND tl_tag.tid IN (" . join($ids, ",") . ")")
 							->execute($objTags->tag, 'tl_calendar_events', time(), time())
 							->fetchAllAssoc());
 						array_push($tags, array('tag_name' => $objTags->tag, 'tag_count' => $count));
@@ -69,7 +69,7 @@ class TagListEvents extends TagList
 		{
 			if (count($this->arrCalendars))
 			{
-				$objTags = $this->Database->prepare("SELECT tag, COUNT(tag) as count FROM tl_tag, tl_calendar_events WHERE tl_tag.id = tl_calendar_events.id AND tl_calendar_events.pid IN ('" . join($this->arrCalendars, "','") . "') AND from_table = ?" . (!BE_USER_LOGGED_IN ? " AND (start='' OR start<?) AND (stop='' OR stop>?) AND published=1" : "") . " GROUP BY tag ORDER BY tag ASC")
+				$objTags = $this->Database->prepare("SELECT tag, COUNT(tag) as count FROM tl_tag, tl_calendar_events WHERE tl_tag.tid = tl_calendar_events.id AND tl_calendar_events.pid IN ('" . join($this->arrCalendars, "','") . "') AND from_table = ?" . (!BE_USER_LOGGED_IN ? " AND (start='' OR start<?) AND (stop='' OR stop>?) AND published=1" : "") . " GROUP BY tag ORDER BY tag ASC")
 					->execute('tl_calendar_events', time(), time());
 				$list = "";
 				$tags = array();
